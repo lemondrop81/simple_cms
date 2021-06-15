bed54998bc0b77ef0911b75d664ce0d29e8a8fb7
Leum <you@example.com>
2021-06-15 14:38:00 -0500

    Store data in cookies

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,10 +1,9 @@
 class AccessController < ApplicationController
 
-  before_action :confirm_logged_in, :except => [:new, :create]
 
   # display menu
   def menu
-    @user_id = session[:user_id]
+    @username = cookies[:username]
   end
 
   # display login form
@@ -14,19 +13,14 @@ class AccessController < ApplicationController
   # process login form
   def create
     # do login process here
-    logger.info("*** Login User #{params[:username]}")
     cookies[:username] = params[:username]
-    session[:user_id] = 1483
-    cookies[:language] = 'en'
-    flash[:notice] = "Log in successful"
     redirect_to(menu_path)
   end
 
   # logout user
   def destroy
     # do logout process here
-    logger.info("*** Logout User #{session[:user_id]}")
-    session[:user_id] = nil
+
     redirect_to(login_path)
   end
 
